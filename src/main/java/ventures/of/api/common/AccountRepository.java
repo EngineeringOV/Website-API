33f8500c9b3fa79de8a1c3ea8aa66befdd64e90c
Einharjar Laptop <engineering@of.ventures>
2022-06-29 18:51:30 +0200

    Disabling leaked account info

--- src/main/java/ventures/of/api/common/AccountRepository.java
+++ src/main/java/ventures/of/api/common/AccountRepository.java
@@ -3,10 +3,12 @@ package ventures.of.api.common;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import ventures.of.api.model.db.Account;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface AccountRepository extends JpaRepository<Account, String>, JpaSpecificationExecutor<Account> {
 
     Account findByUsername(String username);
--- src/main/java/ventures/of/api/common/CharacterRepository.java
+++ src/main/java/ventures/of/api/common/CharacterRepository.java
@@ -2,10 +2,12 @@ package ventures.of.api.common;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import ventures.of.api.model.db.Character;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface CharacterRepository extends JpaRepository<Character, String>, JpaSpecificationExecutor<Character> {
 
     long countByOnlineTrue();
