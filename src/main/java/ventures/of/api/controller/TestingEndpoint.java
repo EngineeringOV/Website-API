6b2e592dec22bfea9d638050d5072e4f0699b24c
Einharjar Laptop <engineering@of.ventures>
2022-06-29 19:40:25 +0200

    New mail method test

--- src/main/java/ventures/of/api/controller/TestingEndpoint.java
+++ src/main/java/ventures/of/api/controller/TestingEndpoint.java
@@ -2,6 +2,8 @@ package ventures.of.api.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 import ventures.of.api.smtp.MailSender;
 import ventures.of.api.smtp.SmtpConfiguration;
@@ -13,17 +15,27 @@ import javax.servlet.http.HttpServletRequest;
 @Log4j2
 public class TestingEndpoint {
 
-    @Autowired
-    private SmtpConfiguration smtpConfiguration;
 
     @Autowired
-    MailSender mailSender;
+    private JavaMailSender javaMailSender;
+
+    void sendEmail(String[] to) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+
+        msg.setTo(to);
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
 
     @GetMapping(value = "")
     @ResponseBody
     public String b() {
         log.info("before mail");
-        mailSender.send("fuck you", "ok im sorry", null, null, "cs.world@of.ventures", "alex.havlund@gmail.com");
+        sendEmail(new String[]{"alex.havlund@gmail.com"});
         log.info("after mail");
         return "you found mee";
     }
@@ -56,9 +68,7 @@ public class TestingEndpoint {
         stringBuilder.append("X-Forwarded-Host = " + request.getHeader("X-Real-IP"));
         stringBuilder.append("\n");
         stringBuilder.append("X-Forwarded-Server = " + request.getHeader("X-Forwarded-For"));
-        stringBuilder.append("\n");
-        stringBuilder.append("smtpConfiguration = " + smtpConfiguration.getHost());
-        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr());
+        stringBuilder.append("<br/>");
         return stringBuilder.toString();
     }
 
--- src/main/java/ventures/of/api/smtp/MailSender.java
+++ src/main/java/ventures/of/api/smtp/MailSender.java
@@ -1,21 +1,10 @@
 package ventures.of.api.smtp;
 
-import java.io.File;
-import java.util.List;
-import java.util.Properties;
-
-import javax.annotation.PostConstruct;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -23,94 +12,17 @@ import org.springframework.stereotype.Component;
 public class MailSender extends JavaMailSenderImpl {
 
     @Autowired
-    private SmtpConfiguration smtpConfiguration;
+    private JavaMailSender javaMailSender;
 
-    @Autowired
-    public MailSender(SmtpConfiguration smtpConfiguration) {
-        this.smtpConfiguration = smtpConfiguration;
-    }
+    void sendEmail() {
 
-    @PostConstruct
-    private void init() {
-        log.debug("Setting MailSender properties from database.");
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("to_1@gmail.com", "to_2@gmail.com", "to_3@yahoo.com");
 
-        setHost(smtpConfiguration.getHost());
-        setPort(smtpConfiguration.getPort());
-        setUsername(smtpConfiguration.getUser());
-        setPassword(smtpConfiguration.getPassword());
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
 
-        Properties properties = new Properties();
-        propertiesPutIfExists(properties, "mail.smtp.auth", smtpConfiguration.getAuth());
-        propertiesPutIfExists(properties, "mail.smtp.starttls.enable", smtpConfiguration.getStls());
-        propertiesPutIfExists(properties, "mail.smtp.ssl.trust", smtpConfiguration.getSsl());
+        javaMailSender.send(msg);
 
-        log.debug("Mail host: {}", smtpConfiguration.getHost());
-        log.debug("Mail port: {}", smtpConfiguration.getPort());
-        log.debug("Mail username: {}", smtpConfiguration.getUser());
-        log.debug("Mail password: REDACTED");
-        log.debug("Mail auth: {}", smtpConfiguration.getAuth());
-        log.debug("Mail stls: {}", smtpConfiguration.getStls());
-        log.debug("Mail ssl: {}", smtpConfiguration.getSsl());
-
-        setJavaMailProperties(properties);
-    }
-
-    /**
-     * Sends an asynchronous E-mail containing the plain, HTML text and attachment.
-     * </p>
-     */
-    @Async
-    public void send(String subject, String plainText, String htmlText, List<File> attachments, String from, String to) {
-        try {
-            MimeMessage message = createMimeMessage(subject, plainText, htmlText, attachments, from, to);
-            log.info("Sending mail");
-            this.send(message);
-        } catch (MailException | MessagingException ex) {
-            log.warn("E-mail failed!", ex);
-        }
-    }
-
-    private void propertiesPutIfExists(Properties properties, String key, Boolean value) {
-        if (value != null) {
-            properties.put(key, value);
-        }
-    }
-
-    /**
-     * Creates a {@code MimeMessage} and copies the template variables.
-     * </p>
-     */
-    private MimeMessage createMimeMessage(String subject, String plainText, String htmlText, List<File> attachments, String from, String to)
-            throws MessagingException {
-        boolean isMultipart = attachments != null && !attachments.isEmpty();
-
-        MimeMessage message = this.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, isMultipart);
-        helper.setTo(to);
-        helper.setFrom(from);
-        helper.setSubject(subject);
-
-        if (plainText != null && htmlText != null) {
-            helper.setText(plainText, htmlText);
-            log.info("Message plain text: {}", plainText);
-            log.info("Message HTML text: {}", htmlText);
-        } else if (htmlText != null) {
-            helper.setText(htmlText, true);
-            log.info("Message HTML text: {}", htmlText);
-        } else if (plainText != null) {
-            helper.setText(plainText, false);
-            log.info("Message plain text: {}", plainText);
-        }
-
-        if (isMultipart) {
-            log.debug("Message is multipart: true");
-            for (File attachment : attachments) {
-                FileSystemResource file = new FileSystemResource(attachment);
-                helper.addAttachment(attachment.getName(), file);
-                log.info("Adding attachment: {}", attachment.getName());
-            }
-        }
-
-        return message;
     }
 }
