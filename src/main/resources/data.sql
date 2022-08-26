7ba984a2f2b51613cdbe49af1457a605ce3b1af8
Einharjar Laptop <engineering@of.ventures>
2022-08-27 00:08:18 +0200

    bump

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA acore_auth;
 CREATE SCHEMA acore_custom;
 
 CREATE TABLE acore_custom.account_reset_request (
-    `uuid`          VARCHAR(30)    primary key,
+    `uuid`        VARCHAR(36)    primary key,
     email         VARCHAR(50)    NOT NULL,
     ip_address    VARCHAR(15)    NOT NULL,
     valid_request TINYINT(1)     NOT NULL,
