0b85042f2c6e2bf43606c54a8a7400400c52f912
Ran Surizon <Engineering@of.ventures>
2026-03-12 16:29:15 +0100

    Fixing nginx to backend communication

--- src/main/java/io/github/engineeringov/website/api/model/api/requests/CreateAccountRequest.java
+++ src/main/java/io/github/engineeringov/website/api/model/api/requests/CreateAccountRequest.java
@@ -1,8 +1,10 @@
 package io.github.engineeringov.website.api.model.api.requests;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateAccountRequest {
     //GenericRequestData
     String captchaToken;
